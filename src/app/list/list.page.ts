6dfe8fd3a4ee7ddbe1a8172d71aa5d4d84c4631e
Victor Liu <victorliu@Victors-MacBook-Pro.local>
2019-03-27 20:42:08 +1100

    Week 2

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../storage.service';
+import { Item } from '../../models/item.model';
+
 
 @Component({
   selector: 'app-list',
@@ -7,16 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
   input:string;
-  listItems: Array<string> = [];
-  constructor() { }
+  listItems: Array<Item> = [];
+
+  constructor(private storage:StorageService) { }
 
   ngOnInit() {
   }
 
-  addListItem(item:string){
-    this.input = '';
-    this.listItems.push(item);
-    // console.log(this.listItems);
-    
-  }  
+  ionViewDidEnter(){
+    this.storage.readData('list')
+    .then( (response:any) => {
+      if( response ){
+        this.listItems = JSON.parse(response);
+      }
+    })
+    .catch( (error) => console.log(error) );
+  }
+
+  addListItem( name:string){
+    this.input = ''; //empty the input 
+    //this.listItems.push(item);
+    // console.log(this.listItems); 
+    let item = {name: name, id: new Date().getTime(), status: false };
+    this.listItems.push( item );
+    this.saveList();
+  } 
+  
+  saveList(){
+    this.storage.saveData('list', this.listItems )
+    .then((response) => {
+      //data written successfully
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  deleteItem(id:number){
+    this.listItems.forEach( (Item, index ) => {
+      if( Item.id == id ){
+        this.listItems.splice( index, 1 );
+      }
+    });
+    this.saveList();
+  }
+
+  changeItemStatus(id:number){
+    this.listItems.forEach( (item) => {
+      if( item.id == id){
+        item.status = ( item.status ==  false )? true : false;
+      }
+    } );
+    this.saveList();
+  }
+  
 }
